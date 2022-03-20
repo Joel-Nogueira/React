b2560d9342878d0c45a2cf0b3fcc9fa8e1a257bb
Joel-Nogueira <joelnogueira733@gmail.com>
2022-03-20 14:59:27 -0300

    Separação das classes em arquivos próprios

---
+++ jogo-da-velha/src/square.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./principal.css";
+
+class Square extends React.Component {
+    render() {
+        return (
+            <button className="square">
+                {/* TODO */}
+            </button>
+        );
+    }
+}
+
+export default Square;
